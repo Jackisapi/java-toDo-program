b6c52e06fd455bb879b3ccf4e2537b3eab07d44e
jack <jack@pop-os.localdomain>
2022-12-27 18:38:47 -0700

    added comments so that code can be read by aliens when they enslave us

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main{
         Scanner toDo = new Scanner(System.in);
         System.out.println("Enter what you have to do today or enter java:exit to exit java:read to read your list or anything input to write to list");
         String toDos = toDo.nextLine();
-
+        // exit command soon to be writen in different class
         if (toDos.equals("java:exit")) {
             System.exit(0);
         }
@@ -18,10 +18,15 @@ public class Main{
     }
         public static void main (String[] args)throws IOException{
             while (true) {
+                // creates the file and sets appending to ture
                 FileWriter list = new FileWriter("list.txt",true);
+                //fetches the date
                 Date today = new Date();
+                //prints
                 System.out.println("Hello todays date is " + today);
+                // calls the function on line 7 NOTE soon to be moved to a class
                 listWrite(list);
+                //closes file
                 list.flush();
                 list.close();
             }
