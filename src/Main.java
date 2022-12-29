1914d1eac9f0830ae2a79a70f90dd0c36a751b91
jackisapi <getphyk3d@gmail.com>
2022-12-29 15:25:24 -0700

    added the java:read function

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,30 @@
 import java.io.IOException;
 import  java.util.Date;
-import java.util.Scanner;
 import java.io.FileWriter;
-public class Main{
+import java.util.Scanner;
+
+public class Main {
     // writes input to list
-        public static void main (String[] args)throws IOException{
-            while (true) {
-                // creates the file and sets appending to ture
-                FileWriter list = new FileWriter("list.txt",true);
-                //fetches the date
-                Date today = new Date();
-                //prints
-                System.out.println("Hello todays date is " + today);
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            // creates the file and sets appending to ture
+            FileWriter list = new FileWriter("list.txt", true);
+            Date today = new Date();
+            System.out.println("Hello todays date is " + today);
+            Scanner usrInput = new Scanner(System.in);
+            String strInput = usrInput.nextLine();
+            if (strInput.equals("java:exit")) {
+                System.exit(1);
+            } else if (strInput.equals("java:read")) {
+                listFun.listRead("list.txt");
+            } else {
                 // calls the function on line 7 NOTE soon to be moved to a class
-                listFun.listWrite(list);
+                listFun.listWrite(list,strInput);
                 //closes file
                 list.flush();
                 list.close();
             }
 
         }
+    }
 }
--- src/listFun.java
+++ src/listFun.java
@@ -1,18 +1,22 @@
+import java.util.Scanner;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
+
 
 public class listFun {
-    static void listWrite(FileWriter doc)throws IOException {
-        Scanner toDo = new Scanner(System.in);
-        System.out.println("Enter what you have to do today or enter java:exit to exit java:read to read your list or anything input to write to list");
-        String toDos = toDo.nextLine();
-        // exit command soon to be writen in different class
-        if (toDos.equals("java:exit")) {
-            System.exit(0);
+    static void listWrite(FileWriter doc, String toDos)throws IOException {
+        doc.append(toDos + "\n");
         }
-        else {
-            doc.append(toDos + "\n");
+        static void listRead(String fileName)throws IOException{
+            FileReader readDoc = new FileReader(fileName);
+            Scanner scanDoc = new Scanner(readDoc);
+            while (scanDoc.hasNextLine()){
+                String listItem = scanDoc.nextLine();
+                System.out.println(listItem);
+            }
+
+
         }
     }
-}
+
